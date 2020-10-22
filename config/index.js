d6d1a362311b0ee11d34f3dc6aff2f278b52ab8d
nunts <sharkrain@outlook.com>
2020-10-22 21:58:48 +0800

    init

---
+++ config/index.js
@@ -0,0 +1,7 @@
+const config = {
+  checkInterval: 1000  * 1,
+  repos: ['https://vip.alisvn.com/xbguan.findsoft_eg_h5/'],
+  username: 'yinzhenyu',
+  password: 'yzy1997YZY',
+}
+module.exports = config;
