19830b7665acdcbb16401aec2b4f4eaee27a9ca1
nunts <sharkrain@outlook.com>
2020-10-26 18:33:35 +0800

    update

--- config/repos.default.js
+++ config/repos.default.js
@@ -3,6 +3,8 @@ module.exports = [
     repo: '', // svn 仓库地址
     firstCheck: true, // 是否为首次检查
     lastCheckTime: new Date().valueOf(), // 最后一次检查更新时间
+    username: '', // 项目用户名，可以为空，配置后会覆盖全局 svn 用户名
+    password: '', // 项目密码，可以为空，配置后会覆盖全局 svn 密码
     svn: 0, // svn 版本号
   },
 ];
